65c3708c4533c38b9ca6ade6c29af4042bc04b4b
TIlves <taniailves@gmail.com>
2018-11-06 21:52:01 +0200

    Add: Toolbar, search button and styles

--- app/src/main/java/ivs/ilves/droidreminder/MainActivity.java
+++ app/src/main/java/ivs/ilves/droidreminder/MainActivity.java
@@ -1,13 +1,38 @@
 package ivs.ilves.droidreminder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+
+    }
+
+    /**
+     * ADD: Toolbar
+     */
+    private void initToolbar() {
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
